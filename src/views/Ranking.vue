55d10e073e00228fb529224210645e0546fa6454
Nelson Chen <crazysim@gmail.com>
2019-02-03 20:17:22 -0800

    Began Stripping

---
+++ src/views/Ranking.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="rankings">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>
+            <abbr title="Position">#</abbr>
+          </th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class Ranking extends Vue {}
+</script>
