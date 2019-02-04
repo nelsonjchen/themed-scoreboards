e06a82939a0aa030e6ae32ecdef4921e1bb02afc
Nelson Chen <crazysim@gmail.com>
2019-02-03 22:16:26 -0800

    hook up firebase

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyALa4tI6lUvDRpa8Dc9K-Ehau36cfMlVEI',
+  authDomain: 'themed-scoreboards.firebaseapp.com',
+  databaseURL: 'https://themed-scoreboards.firebaseio.com',
+  projectId: 'themed-scoreboards',
+  storageBucket: 'themed-scoreboards.appspot.com',
+  messagingSenderId: '1051809615125'
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
